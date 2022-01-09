7022644c901def4cef0a4e0399dcc1120be8c38e
Liam Douglass <douglass.l@northeastern.edu>
2022-01-08 19:43:45 -0500

    finished implementing stream listener

--- live_trader/stream_listener.py
+++ live_trader/stream_listener.py
@@ -1,18 +1,28 @@
 from utils.utils import get_logger
+from datetime import datetime
+from utils.quote import Quote
 
 
 class StreamListener:
-    def __init__(self, stream, indicator_queue, trade_update_queue):
+    def __init__(self, stream, signal_queue, trade_update_queue, strategies, period_aggregators):
         self.stream = stream
-        self.indicator_queue = indicator_queue
+        self.signal_queue = signal_queue
         self.trade_update_queue = trade_update_queue
-        self.strategies = {}
+        self.strategies = strategies
+        self.period_aggregators = period_aggregators
         self.quote_counts = {}
         self.period_counts = {}
         self.logger = get_logger("stream_listener")
 
-    def trade_update_callback(self, tu):
-        pass
+    def trade_update_callback(self, trade_update):
+        order = trade_update.order
+        trade_update_msg = {
+            "symbol": order.symbol,
+            "order_id": order.id,
+            "event": trade_update.event,
+            "timestamp": trade_update.timestamp
+        }
+        self.trade_update_queue.put(trade_update_msg)
 
     def get_quote_call_back(self, symbol):
         async def quote_callback(q):
@@ -21,37 +31,36 @@ class StreamListener:
         return quote_callback
 
     def process_quote(self, api_q, symbol):
-        if symbol not in self.quote_counts:
-            self.quote_counts[symbol] = 0
+        self.quote_counts[symbol] = self.quote_counts.get(symbol, 0) + 1
 
-        if symbol not in self.per_counts:
-            self.per_counts[symbol] = 0
-        self.quote_counts[symbol] += 1
+        if symbol not in self.period_counts:
+            self.period_counts[symbol] = 0
 
-        # dt = parse_timestamp(api_q.timestamp)
         dt = datetime.utcfromtimestamp(api_q.timestamp.timestamp())
         quote = Quote(dt, float(api_q.ask_price), int(api_q.ask_size), float(api_q.bid_price), int(api_q.bid_size))
 
         strategy = self.strategies[symbol]
-        period_aggregator = self.per_aggs[symbol]
-
-        if strategy.state == "tracking" and dt.time() >= time(hour=14, minute=30):
-            strategy.state = "buy"
+        period_aggregator = self.period_aggregators[symbol]
 
         finished_period = period_aggregator.process_quote(quote)
         if finished_period:
-            self.per_counts[symbol] += 1
-            self.logger.info(f"FINISHED {symbol} PERIOD {self.per_counts[symbol]}, {self.quote_counts[symbol]} QUOTES")
+            self.period_counts[symbol] = self.period_counts.get(symbol, 0) + 1
+            self.logger.info(f"FINISHED {symbol} PERIOD {self.period_counts[symbol]}, {self.quote_counts[symbol]} QUOTES")
             self.quote_counts[symbol] = 0
-            strategy.update_analyzer_vals()
+            strategy.update_analyzer_vals(period_aggregator)
 
-            try:
-                old_state = strategy.state
-                strategy.make_decision()
-                self.logger.debug(f"PSAR MA Cross {symbol} made decision {old_state} -> {strategy.state}")
-            except TooManyPositionsError:
-                pass
+            signal = strategy.generate_signal()
+            if signal is not None:
+                signal_message = {
+                    "symbol": symbol,
+                    "type": signal,
+                    "strength": None,  # TODO
+                    "timestamp": dt
+                }
+                self.signal_queue.put(signal_message)
 
     def start(self):
         self.stream.subscribe_trade_updates(self.trade_update_callback)
+        for symbol in self.strategies.keys():
+            self.stream.subscribe_quotes(symbol, self.get_quote_call_back(symbol))
         self.stream.run()
